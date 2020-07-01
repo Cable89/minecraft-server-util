e0c7cda0c9e8d3a829c7087e09a33f11a826cf81
Øystein Smith <oystein@infusion.ed.ntnu.no>
2020-07-01 18:41:59 +0200

    No longer rendering separate 1.13

--- overviewer.sh
+++ overviewer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#overviewer.py --config=/etc/minecraft/options.py
-/etc/minecraft/overviewer113/overviewer.py --config=/etc/minecraft/options.py --genpoi --skip-scan
+overviewer.py --config=/etc/minecraft/options.py --genpoi --skip-scan
+overviewer.py --config=/etc/minecraft/options.py
+#/etc/minecraft/overviewer113/overviewer.py --config=/etc/minecraft/options.py --genpoi --skip-scan
 #/etc/minecraft/overviewer113/overviewer.py --config=/etc/minecraft/options.py
