3ea72dc20ba83ff6bdfe90fbbab3ee5dee857745
Øystein Smith <oystein@infusion.ed.ntnu.no>
2020-07-01 18:42:22 +0200

    Added explorationstations

--- stations.py
+++ stations.py
@@ -4,6 +4,10 @@ def filterfunction(poi):
     if poi['id'] == 'Station':
         return poi['name']
 
+def explorationfilterfunction(poi):
+    if poi['id'] == 'Exploration Station':
+        return poi['name']
+
 overworld1  = {'id': 'Station',
                'x':-4096,
                'y':80,
@@ -45,6 +49,24 @@ overworld8 = {'id': 'Station',
               'z':0,
               'name':'Serengeti Sentralstasjon'}
 
+
+exploration01 = {'id': 'Exploration Station', 'x':-8192, 'y':80, 'z':-8192, 'name':'White Exploration Station'}
+exploration02 = {'id': 'Exploration Station', 'x':-4096, 'y':80, 'z':-8192, 'name':'Grey Exploration Station'}
+exploration03 = {'id': 'Exploration Station', 'x':    0, 'y':80, 'z':-8192, 'name':'Dark Grey Exploration Station'}
+exploration04 = {'id': 'Exploration Station', 'x': 4096, 'y':80, 'z':-8192, 'name':'Black Exploration Station'}
+exploration05 = {'id': 'Exploration Station', 'x': 8192, 'y':80, 'z':-8192, 'name':'Brown Exploration Station'}
+exploration06 = {'id': 'Exploration Station', 'x': 8192, 'y':80, 'z':-4096, 'name':'Red Exploration Station'}
+exploration07 = {'id': 'Exploration Station', 'x': 8192, 'y':80, 'z':    0, 'name':'Orange Exploration Station'}
+exploration08 = {'id': 'Exploration Station', 'x': 8192, 'y':80, 'z': 4096, 'name':'Yellow Exploration Station'}
+exploration09 = {'id': 'Exploration Station', 'x': 8192, 'y':80, 'z': 8192, 'name':'Green Exploration Station'}
+exploration10 = {'id': 'Exploration Station', 'x': 4096, 'y':80, 'z': 8192, 'name':'Dark Green Exploration Station'}
+exploration11 = {'id': 'Exploration Station', 'x':    0, 'y':80, 'z': 8192, 'name':'Cyan Exploration Station'}
+exploration12 = {'id': 'Exploration Station', 'x':-4096, 'y':80, 'z': 8192, 'name':'Light Blue Exploration Station'}
+exploration13 = {'id': 'Exploration Station', 'x':-8192, 'y':80, 'z': 8192, 'name':'Blue Exploration Station'}
+exploration14 = {'id': 'Exploration Station', 'x':-8192, 'y':80, 'z': 4096, 'name':'Purple Exploration Station'}
+exploration15 = {'id': 'Exploration Station', 'x':-8192, 'y':80, 'z':    0, 'name':'Magenta Exploration Station'}
+exploration16 = {'id': 'Exploration Station', 'x':-8192, 'y':80, 'z':-4096, 'name':'Pink Exploration Station'}
+
 overworld = [overworld1,
              overworld2,
              overworld3,
@@ -54,6 +76,25 @@ overworld = [overworld1,
              overworld7,
              overworld8]
 
+exploration = [exploration01,
+               exploration02,
+               exploration03,
+               exploration04,
+               exploration05,
+               exploration06,
+               exploration07,
+               exploration08,
+               exploration09,
+               exploration10,
+               exploration11,
+               exploration12,
+               exploration13,
+               exploration14,
+               exploration15,
+               exploration16]
+
+overworld.extend(exploration)
+
 netherhub = {'id': 'Station',
              'x':0,
              'y':120,
@@ -62,8 +103,10 @@ netherhub = {'id': 'Station',
 
 nether = copy.deepcopy(overworld)
 nether.append(netherhub)
+nether.extend(copy.deepcopy(exploration))
 
 for station in nether:
+    station['id'] = 'Station'
     station['x'] = station['x']/8
     station['y'] = 120
     station['z'] = station['z']/8
